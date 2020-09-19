90f1e7f0a156e303603a3129e906e1d2b07f6304
turubee <turubee1018@gmail.com>
2020-09-20 01:06:06 +0900

    changed to use Brewfile

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# mac provisioning playbook
+# mac provisioning
 
 ```bash
 $ xcode-select --install
 $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-$ brew doctor
-$ brew update
-$ brew install ansible
 $ git clone https://github.com/turubee/mac_provisioning
 $ cd mac_provisioning
-$ bash provision.sh
+$ ./provision.sh
 ```
--- provision.sh
+++ provision.sh
@@ -1,3 +1,27 @@
-#!/bin/bash
+#!/bin/zsh
 mkdir -p logs
-ansible-playbook -i "localhost," mac.yaml --diff | tee logs/$0_$(date '+%Y%m%d%H%M%S').log
+LOG_FILE_PATH=logs/$0_$(date '+%Y%m%d%H%M%S').log
+
+echo "### start brew bundle"
+brew bundle -v | tee -a ${LOG_FILE_PATH}
+
+echo "\n### start brew bundle cleanup"
+OUTPUT=$(brew bundle cleanup)
+echo "${OUTPUT}\n"
+if [[ ${OUTPUT} =~ "Would uninstall casks" ]]; then
+    read yn\?'cleanup execute? (y/n): '
+    case "${yn}" in
+    [yY]*)
+        brew bundle cleanup -v -f | tee -a ${LOG_FILE_PATH}
+        ;;
+    *)
+        echo "abort."
+        exit 128
+        ;;
+    esac
+else
+    echo "no cleanup target."
+fi
+
+echo "\n### start ansible-playbook"
+ansible-playbook -i "localhost," mac.yaml --diff | tee -a ${LOG_FILE_PATH}
